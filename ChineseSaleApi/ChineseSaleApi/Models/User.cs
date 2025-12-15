fa786ccecb6c2339c63803e8e6464a64057dad18
Dvori-588 <d0556747558@gmail.com>
2025-12-15 23:55:31 +0200

    first update

---
+++ ChineseSaleApi/ChineseSaleApi/Models/User.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChineseSaleApi.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string UserName { get; set; }
+        [Required]
+        [MaxLength(15)]
+        public string Password { get; set; }
+        [MaxLength(30)]
+        public string? FirstName { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string LastName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Phone]
+        public string? Phone { get; set; }
+
+        public bool IsAdmin { get; set; } = false;
+
+        public int AddressId { get; set; }
+        public Address? Address { get; set; }
+
+        public ICollection<Gift>? WonGits { get; set; }
+        public ICollection<Gift>? InvitedGifts { get; set; }
+
+    }
+}
